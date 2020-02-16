401e7553fdc7f17578cdba8d3d8cff03c100071a
francisguchie <francisguchie@gmail.com>
2020-02-16 22:53:57 +0000

    ebridgeAfrica added debug print 5

--- src/main/java/org/fineract/messagegateway/sms/providers/impl/wirepick/smsgateway/utility/HttpUrls.java
+++ src/main/java/org/fineract/messagegateway/sms/providers/impl/wirepick/smsgateway/utility/HttpUrls.java
@@ -22,7 +22,11 @@
 package org.fineract.messagegateway.sms.providers.impl.wirepick.smsgateway.Utility;
 
 import java.io.StringReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
