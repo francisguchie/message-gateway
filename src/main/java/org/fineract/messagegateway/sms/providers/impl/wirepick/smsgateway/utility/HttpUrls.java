bcedbbd6593165362f776d03989b1d7d3a48fec9
francisguchie <francisguchie@gmail.com>
2020-02-17 01:45:38 +0000

    ebridgeAfrica doing json Api 1

--- src/main/java/org/fineract/messagegateway/sms/providers/impl/wirepick/smsgateway/utility/HttpUrls.java
+++ src/main/java/org/fineract/messagegateway/sms/providers/impl/wirepick/smsgateway/utility/HttpUrls.java
@@ -45,7 +45,6 @@ import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.cookie.CookiePolicy;
 import org.apache.commons.httpclient.params.HttpClientParams;
 
-import org.apache.commons.io.IOUtils;
 
 
 import org.fineract.messagegateway.sms.providers.impl.wirepick.smsgateway.model.MsgStatus;
