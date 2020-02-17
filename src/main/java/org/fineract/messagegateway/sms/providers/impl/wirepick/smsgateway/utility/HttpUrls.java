75d718a98a49ca5f1ddce61d21ebc7fe67baa530
francisguchie <francisguchie@gmail.com>
2020-02-17 17:27:37 +0000

    ebridgeAfrica doing json Api 1

--- src/main/java/org/fineract/messagegateway/sms/providers/impl/wirepick/smsgateway/utility/HttpUrls.java
+++ src/main/java/org/fineract/messagegateway/sms/providers/impl/wirepick/smsgateway/utility/HttpUrls.java
@@ -71,7 +71,7 @@ public class HttpUrls {
 		PostMethod postMethod = new PostMethod(sUrl);
 		postMethod.setRequestHeader("Accept-Charset", "UTF-8");
 
-		Settings.printJsonDataMitData(config);
+		Settings.printJsonDataMitData();
 
 		if (headers != null && !headers.isEmpty()) {
 			for (Entry<String, String> entry : headers.entrySet()) {
